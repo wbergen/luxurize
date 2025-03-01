3d9b0f8b28ecb634fe2a017f9c4578d6ff01afc4
Will <william.g.bergen@gmail.com>
2025-03-01 14:23:48 -0500

    initial commit

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        // Find or create the user
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'google_id' => $googleUser->getId(),
+                'avatar' => $googleUser->getAvatar(),
+                'password' => bcrypt(str()->random(24)), // Random password as it's not used
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard'); // Change to your desired route
+    }
+}
