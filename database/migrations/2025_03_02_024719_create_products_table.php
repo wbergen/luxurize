8143d6bf83aa40504913dffe6d92a891570ede86
Will <william.g.bergen@gmail.com>
2025-03-01 22:48:47 -0500

    bootstrapping

--- database/migrations/2025_03_02_024719_create_products_table.php
+++ database/migrations/2025_03_02_024719_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 128);
-            $table->string('description', 256);
+            $table->text('description');
             $table->decimal('price', 8, 2);
             $table->string('for_sale');
             $table->string('image', 256);
